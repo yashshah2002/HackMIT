3ebba1d9ed1078a41daa9e0e955044ab94fa84fe
SaloniSS <sxs170015@utdallas.edu>
2020-09-19 21:16:54 -0500

    WIP: Getting cloud function to fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  let [textToAnalyze, setTextToAnalyze] = useState("");
+
+  const analyzeText = async () => {
+    console.log("Analyzing", textToAnalyze);
+    const response = await axios.get(
+      `https://us-central1-saloni-shivdasani.cloudfunctions.net/subjectivity-analyzer?text=${textToAnalyze}`,
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+      }
+    );
+    console.log(response);
+  };
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setTextToAnalyze(newValue);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +39,8 @@ function App() {
         >
           Learn React
         </a>
+        <input type="text" value={textToAnalyze} onChange={handleChange} />
+        <button onClick={() => analyzeText()}>Analyze</button>
       </header>
     </div>
   );
