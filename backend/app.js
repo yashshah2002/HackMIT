03dc68f059a0251ed6a907c94d2003429eb05d93
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-09-19 21:19:31 -0500

    basic backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ test: "hi" });
+});
+
+app.listen(5000);
