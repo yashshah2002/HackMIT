2ebf7dfa1a922c2fc02ca4e4f0bb897a8463c78e
SaloniSS <sxs170015@utdallas.edu>
2020-09-20 00:31:49 -0500

    fixed backend error

--- backend/routes/subjectivity-routes.js
+++ backend/routes/subjectivity-routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
-  const textToAnalyze = req.params.text;
+  const textToAnalyze = req.query.text;
+  //console.log(req.query.text);
   const response = await axios.get(
     `https://us-central1-saloni-shivdasani.cloudfunctions.net/subjectivity-analyzer?text=${textToAnalyze}`
   );
-  console.log(response.data);
+  //console.log(response);
   return res.status(200).json(response.data);
 });
 
